8f1d852df8ea40b4184ea495c72588ea801a3057
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2021-09-22 16:14:40 -0400

    Add pinout and pin definitions

--- src/config.h
+++ src/config.h
@@ -1,8 +1,24 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/************ TCP/IP settings ****************/
 #define TCP_PORT 4533   // default for rotctld
 
+/************ PIN definitions ****************/
+// Outputs movement
+#define MRIGHT D5
+#define MLEFT D6
+#define MUP D7
+#define MDOWN D8
 
+// Input movement interrupts
+#define IIAZ SD3
+#define IIEL SD2
+
+// movement current sensor
+#define ISMOVING D1
+
+// Rotor transformer relay
+#define TRAFOON D2
 
 #endif // CONFIG_H
